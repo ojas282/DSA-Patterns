a41a1e615e031c86b51091a302ae1b5b1471bdcf
ojas282 <ojaskumargupta28@gmail.com>
2024-07-14 13:40:49 +0530

    Updated

--- Patterns.py
+++ Patterns.py
@@ -82,3 +82,22 @@ OP:
 # # 
 # 
 '''
+def left_90(n):
+  '''for i in range(1,n+1):
+    print(" " * (n-i)+ "#" * i)'''
+  
+  for i in range(1,n+1):
+    for j in range(n-i):
+      print(" ",end="")
+    for k in range(i):
+      print("#",end="")
+    print()
+left_90(5)
+'''
+OP:
+    #
+   ##
+  ###
+ ####
+#####
+'''
